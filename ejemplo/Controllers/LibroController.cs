e4466611240181b8fa935d7fa721038f4419e7c0
Sebastian Molina <sebam89@hotmail.com>
2018-10-24 19:34:29 -0300

    Start

---
+++ ejemplo/Controllers/LibroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ejemplo.Controllers
+{
+    public class LibroController : Controller
+    {
+        // GET: Libro
+        public ActionResult Listar()
+        {
+            return View();
+        }
+    }
+}
---
+++ ejemplo/Controllers/UsuarioController.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ejemplo.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public ActionResult Listar()
+        {
+            return View();
+        }
+
+        public ActionResult NuevoUsuario()
+        {
+            @ViewBag.Title = "Nuevo Usuario";
+            return View();
+        }
+
+    }
+}
